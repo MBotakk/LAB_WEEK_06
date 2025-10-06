7d8e5128c23d9299c63f3f5a387f936131463233
MBotakk <samuel.itachis4@gmail.com>
2025-10-06 20:32:31 +0700

    Commit No.4 - Assigment

--- app/src/main/java/com/samuel/lab_week_06/MainActivity.kt
+++ app/src/main/java/com/samuel/lab_week_06/MainActivity.kt
@@ -2,7 +2,7 @@ package com.samuel.lab_week_06
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.ItemTouchHelper // Import ItemTouchHelper
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.samuel.lab_week_06.model.CatBreed
@@ -21,9 +21,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         recyclerView.adapter = catAdapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
         val itemTouchHelper = ItemTouchHelper(catAdapter.getSwipeCallback())
         itemTouchHelper.attachToRecyclerView(recyclerView)
 
@@ -52,6 +52,55 @@ class MainActivity : AppCompatActivity() {
                 name = "Curious George",
                 biography = "Award winning investigator.",
                 imageUrl = "https://cdn2.thecatapi.com/images/bar.jpg"
+            ),
+            CatModel(
+                gender = Gender.Male,
+                breed = CatBreed.BalineseJavanese,
+                name = "Leo",
+                biography = "King of the living room jungle.",
+                imageUrl = "https://cdn2.thecatapi.com/images/c4h.jpg"
+            ),
+            CatModel(
+                gender = Gender.Female,
+                breed = CatBreed.BalineseJavanese,
+                name = "Luna",
+                biography = "Enjoys staring contests and midnight zoomies.",
+                imageUrl = "https://cdn2.thecatapi.com/images/4TN-Zl2-h.jpg"
+            ),
+            CatModel(
+                gender = Gender.Male,
+                breed = CatBreed.ExoticShorthair,
+                name = "Milo",
+                biography = "A foodie and a professional napper.",
+                imageUrl = "https://cdn2.thecatapi.com/images/j6.jpg"
+            ),
+            CatModel(
+                gender = Gender.Female,
+                breed = CatBreed.AmericanCurl,
+                name = "Chloe",
+                biography = "Elegant, sophisticated, and loves knocking things off tables.",
+                imageUrl = "https://cdn2.thecatapi.com/images/aax.jpg"
+            ),
+            CatModel(
+                gender = Gender.Male,
+                breed = CatBreed.ExoticShorthair,
+                name = "Simba",
+                biography = "Believes every box is his new kingdom.",
+                imageUrl = "https://cdn2.thecatapi.com/images/b29.jpg"
+            ),
+            CatModel(
+                gender = Gender.Female,
+                breed = CatBreed.BalineseJavanese,
+                name = "Daisy",
+                biography = "Loves chasing sunbeams and chirping at birds.",
+                imageUrl = "https://cdn2.thecatapi.com/images/bnt.jpg"
+            ),
+            CatModel(
+                gender = Gender.Male,
+                breed = CatBreed.ExoticShorthair,
+                name = "Ollie",
+                biography = "A bit clumsy, but has a heart of gold.",
+                imageUrl = "https://cdn2.thecatapi.com/images/dn9.jpg"
             )
         )
     }
